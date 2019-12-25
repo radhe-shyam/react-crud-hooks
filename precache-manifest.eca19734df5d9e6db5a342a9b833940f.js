6b2831956cc339aab323f9542a6ae93fd561e794
radhe-shyam <jangir.radheyshyam@gmail.com>
2019-12-25 20:42:06 +0530

    Updates

---
+++ precache-manifest.eca19734df5d9e6db5a342a9b833940f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062395c286a220edc772c3749b0c1846",
+    "url": "/react-crud-hooks/index.html"
+  },
+  {
+    "revision": "7f1e1898a8e49f2c45d3",
+    "url": "/react-crud-hooks/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "e3bbe086b7ee6d89e2ee",
+    "url": "/react-crud-hooks/static/js/2.6c416b80.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-crud-hooks/static/js/2.6c416b80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f1e1898a8e49f2c45d3",
+    "url": "/react-crud-hooks/static/js/main.8db095eb.chunk.js"
+  },
+  {
+    "revision": "ff1d202825f72db4bbb6",
+    "url": "/react-crud-hooks/static/js/runtime-main.1da43876.js"
+  }
+]);
